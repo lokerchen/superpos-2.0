a307ee1136db092a17992424162d09ed8f52d9b6
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2017-05-05 16:34:22 +0000

    增加用户基本信息基础类UsrBase

---
+++ SuperPOS/trunk/SuperPOS.Domain/Entities/UsrBaseInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SuperPOS.Domain.Entities
+{
+    public class UsrBaseInfo
+    {
+        [XmlElement(ElementName = "SystemKey")]
+        public virtual Guid SystemKey { get; set; }
+
+        //用户编号
+        [XmlElement(ElementName = "UsrID")]
+        public virtual string UsrID { get; set; }
+
+        //用户名
+        [XmlElement(ElementName = "UsrName")]
+        public virtual string UsrName { get; set; }
+
+        //用户密码
+        [XmlElement(ElementName = "UsrPwd")]
+        public virtual string UsrPwd { get; set; }
+
+        //备注
+        [XmlElement(ElementName = "UsrRmk")]
+        public virtual string UsrRmk { get; set; }
+    }
+}
