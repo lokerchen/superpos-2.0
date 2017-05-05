092aba18c5141d6eafd3eca8d0c5c30fe5037edc
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2017-05-05 14:50:46 +0000

    调整显示位置

--- SuperPOS/trunk/SuperPOS/UI/FrmLogon.Designer.cs
+++ SuperPOS/trunk/SuperPOS/UI/FrmLogon.Designer.cs
@@ -65,7 +65,7 @@
             // 
             this.txtPwd.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txtPwd.EditValue = "PASSWORD";
-            this.txtPwd.Location = new System.Drawing.Point(478, 165);
+            this.txtPwd.Location = new System.Drawing.Point(478, 166);
             this.txtPwd.Name = "txtPwd";
             this.txtPwd.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPwd.Properties.Appearance.Options.UseFont = true;
@@ -77,7 +77,7 @@
             // 
             this.labelControl1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelControl1.Location = new System.Drawing.Point(372, 171);
+            this.labelControl1.Location = new System.Drawing.Point(372, 172);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(103, 28);
             this.labelControl1.TabIndex = 11;
@@ -99,7 +99,7 @@
             this.groupBox1.Controls.Add(this.btn3);
             this.groupBox1.Controls.Add(this.btn2);
             this.groupBox1.Controls.Add(this.btn1);
-            this.groupBox1.Location = new System.Drawing.Point(367, 205);
+            this.groupBox1.Location = new System.Drawing.Point(367, 201);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(336, 227);
             this.groupBox1.TabIndex = 10;
@@ -110,7 +110,7 @@
             this.btn1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btn1.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn1.Appearance.Options.UseFont = true;
-            this.btn1.Location = new System.Drawing.Point(6, 14);
+            this.btn1.Location = new System.Drawing.Point(6, 7);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(60, 45);
             this.btn1.TabIndex = 0;
@@ -123,7 +123,7 @@
             this.groupControl1.Controls.Add(this.lblTime);
             this.groupControl1.Controls.Add(this.labelControl5);
             this.groupControl1.Controls.Add(this.labelControl3);
-            this.groupControl1.Location = new System.Drawing.Point(367, 26);
+            this.groupControl1.Location = new System.Drawing.Point(367, 22);
             this.groupControl1.Name = "groupControl1";
             this.groupControl1.Size = new System.Drawing.Size(336, 133);
             this.groupControl1.TabIndex = 9;
@@ -132,7 +132,7 @@
             // 
             this.lblSession.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblSession.Appearance.Font = new System.Drawing.Font("微软雅黑", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lblSession.Location = new System.Drawing.Point(139, 77);
+            this.lblSession.Location = new System.Drawing.Point(139, 70);
             this.lblSession.Name = "lblSession";
             this.lblSession.Size = new System.Drawing.Size(99, 36);
             this.lblSession.TabIndex = 5;
@@ -142,7 +142,7 @@
             // 
             this.lblTime.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblTime.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTime.Location = new System.Drawing.Point(206, 30);
+            this.lblTime.Location = new System.Drawing.Point(206, 23);
             this.lblTime.Name = "lblTime";
             this.lblTime.Size = new System.Drawing.Size(102, 30);
             this.lblTime.TabIndex = 4;
@@ -152,7 +152,7 @@
             // 
             this.labelControl5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelControl5.Appearance.Font = new System.Drawing.Font("微软雅黑", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.labelControl5.Location = new System.Drawing.Point(23, 77);
+            this.labelControl5.Location = new System.Drawing.Point(23, 70);
             this.labelControl5.Name = "labelControl5";
             this.labelControl5.Size = new System.Drawing.Size(110, 36);
             this.labelControl5.TabIndex = 3;
@@ -162,7 +162,7 @@
             // 
             this.labelControl3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelControl3.Location = new System.Drawing.Point(23, 30);
+            this.labelControl3.Location = new System.Drawing.Point(23, 23);
             this.labelControl3.Name = "labelControl3";
             this.labelControl3.Size = new System.Drawing.Size(130, 30);
             this.labelControl3.TabIndex = 1;
@@ -171,7 +171,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox2.Location = new System.Drawing.Point(36, 242);
+            this.pictureBox2.Location = new System.Drawing.Point(36, 238);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(297, 188);
             this.pictureBox2.TabIndex = 8;
@@ -180,7 +180,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox1.Location = new System.Drawing.Point(36, 26);
+            this.pictureBox1.Location = new System.Drawing.Point(36, 22);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(297, 183);
             this.pictureBox1.TabIndex = 7;
@@ -191,7 +191,7 @@
             this.btn2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btn2.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn2.Appearance.Options.UseFont = true;
-            this.btn2.Location = new System.Drawing.Point(76, 14);
+            this.btn2.Location = new System.Drawing.Point(76, 7);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(60, 45);
             this.btn2.TabIndex = 1;
@@ -202,7 +202,7 @@
             this.btn3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btn3.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn3.Appearance.Options.UseFont = true;
-            this.btn3.Location = new System.Drawing.Point(146, 14);
+            this.btn3.Location = new System.Drawing.Point(146, 7);
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(60, 45);
             this.btn3.TabIndex = 2;
@@ -213,7 +213,7 @@
             this.btn6.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btn6.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn6.Appearance.Options.UseFont = true;
-            this.btn6.Location = new System.Drawing.Point(146, 69);
+            this.btn6.Location = new System.Drawing.Point(146, 62);
             this.btn6.Name = "btn6";
             this.btn6.Size = new System.Drawing.Size(60, 45);
             this.btn6.TabIndex = 5;
@@ -224,7 +224,7 @@
             this.btn5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btn5.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn5.Appearance.Options.UseFont = true;
-            this.btn5.Location = new System.Drawing.Point(76, 69);
+            this.btn5.Location = new System.Drawing.Point(76, 62);
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(60, 45);
             this.btn5.TabIndex = 4;
@@ -235,7 +235,7 @@
             this.btn4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btn4.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn4.Appearance.Options.UseFont = true;
-            this.btn4.Location = new System.Drawing.Point(6, 69);
+            this.btn4.Location = new System.Drawing.Point(6, 62);
             this.btn4.Name = "btn4";
             this.btn4.Size = new System.Drawing.Size(60, 45);
             this.btn4.TabIndex = 3;
@@ -246,7 +246,7 @@
             this.btn9.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btn9.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn9.Appearance.Options.UseFont = true;
-            this.btn9.Location = new System.Drawing.Point(146, 124);
+            this.btn9.Location = new System.Drawing.Point(146, 117);
             this.btn9.Name = "btn9";
             this.btn9.Size = new System.Drawing.Size(60, 45);
             this.btn9.TabIndex = 8;
@@ -257,7 +257,7 @@
             this.btn8.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btn8.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn8.Appearance.Options.UseFont = true;
-            this.btn8.Location = new System.Drawing.Point(76, 124);
+            this.btn8.Location = new System.Drawing.Point(76, 117);
             this.btn8.Name = "btn8";
             this.btn8.Size = new System.Drawing.Size(60, 45);
             this.btn8.TabIndex = 7;
@@ -268,7 +268,7 @@
             this.btn7.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btn7.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn7.Appearance.Options.UseFont = true;
-            this.btn7.Location = new System.Drawing.Point(6, 124);
+            this.btn7.Location = new System.Drawing.Point(6, 117);
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(60, 45);
             this.btn7.TabIndex = 6;
@@ -279,7 +279,7 @@
             this.btnOK.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnOK.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOK.Appearance.Options.UseFont = true;
-            this.btnOK.Location = new System.Drawing.Point(216, 14);
+            this.btnOK.Location = new System.Drawing.Point(216, 7);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(110, 100);
             this.btnOK.TabIndex = 9;
@@ -290,7 +290,7 @@
             this.btnExit.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnExit.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExit.Appearance.Options.UseFont = true;
-            this.btnExit.Location = new System.Drawing.Point(216, 124);
+            this.btnExit.Location = new System.Drawing.Point(216, 117);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(110, 96);
             this.btnExit.TabIndex = 10;
@@ -301,7 +301,7 @@
             this.btnClear.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnClear.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClear.Appearance.Options.UseFont = true;
-            this.btnClear.Location = new System.Drawing.Point(113, 175);
+            this.btnClear.Location = new System.Drawing.Point(113, 168);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(93, 45);
             this.btnClear.TabIndex = 12;
@@ -312,7 +312,7 @@
             this.btn0.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btn0.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn0.Appearance.Options.UseFont = true;
-            this.btn0.Location = new System.Drawing.Point(6, 175);
+            this.btn0.Location = new System.Drawing.Point(6, 168);
             this.btn0.Name = "btn0";
             this.btn0.Size = new System.Drawing.Size(101, 45);
             this.btn0.TabIndex = 11;
@@ -327,7 +327,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(747, 445);
+            this.ClientSize = new System.Drawing.Size(747, 450);
             this.Controls.Add(this.txtPwd);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.groupBox1);
