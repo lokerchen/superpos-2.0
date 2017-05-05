4d4843185d5c1b42a4466de5bf09edb727cec248
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2017-05-05 15:50:23 +0000

    增加代码备注

--- SuperPOS/trunk/SuperPOS.Domain/Entities/TAShiftCodeInfo.cs
+++ SuperPOS/trunk/SuperPOS.Domain/Entities/TAShiftCodeInfo.cs
@@ -8,27 +8,35 @@ namespace SuperPOS.Domain.Entities
         [XmlElement(ElementName = "SystemKey")]
         public virtual Guid SystemKey { get; set; }
 
+        //时段编号
         [XmlElement(ElementName = "ShiftCode")]
         public virtual string ShiftCode { get; set; }
 
+        //时段英文名称
         [XmlElement(ElementName = "ShiftName")]
         public virtual string ShiftName { get; set; }
 
+        //时段其他名称
         [XmlElement(ElementName = "OtherName")]
         public virtual string OtherName { get; set; }
 
+        //是否具有特殊时段
         [XmlElement(ElementName = "IsSpecial")]
         public virtual string IsSpecial { get; set; }
 
+        //特殊时段开始
         [XmlElement(ElementName = "DtFrom")]
         public virtual string DtFrom { get; set; }
 
+        //特殊时段结束
         [XmlElement(ElementName = "DtEnd")]
         public virtual string DtEnd { get; set; }
 
+        //特殊时段内容
         [XmlElement(ElementName = "SpecialContent")]
         public virtual string SpecialContent { get; set; }
 
+        //备注
         [XmlElement(ElementName = "ShiftRmk")]
         public virtual string ShiftRmk { get; set; }
     }
