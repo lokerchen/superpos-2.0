e7ea79a73fd77737e0797e9fb61fc5c651831363
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2017-05-06 01:57:06 +0000

    增加用户权限类UsrAuthority

--- SuperPOS/trunk/SuperPOS/Common/CommonData.cs
+++ SuperPOS/trunk/SuperPOS/Common/CommonData.cs
@@ -8,5 +8,7 @@ namespace SuperPOS.Common
         public static IList<TAShiftCodeInfo> TaShiftCodeList { get; set; } 
 
         public static IList<UsrBaseInfo> UsrBase { get; set; } 
+
+        public static IList<UsrAuthorityInfo> UsrAuthority { get; set; }
     }
 }
