6234d07494792e0c44af3c84657c0d481e1809ae
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2017-05-10 14:46:44 +0000

    增加工具类，并添加弹出窗口方法

---
+++ SuperPOS/trunk/SuperPOS/Common/CommonTool.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace SuperPOS.Common
+{
+    public class CommonTool
+    {
+        #region 确认对话框
+        /// <summary>
+        /// 确认对话框
+        /// </summary>
+        /// <param name="strInfo">提示信息</param>
+        public static void ShowMessage(string strInfo)
+        {
+            XtraMessageBox.Show(strInfo, "Infomation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        #endregion
+
+        #region 提示对话框
+        /// <summary>
+        /// 提示对话框
+        /// </summary>
+        /// <returns></returns>
+        public static DialogResult ConfirmDelete()
+        {
+            return XtraMessageBox.Show("Are you sure delete this record?", "Infomation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+        }
+        #endregion
+    }
+}
