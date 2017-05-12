6fe30afc8370628c14c3512a0fd5ffdc3a7fa132
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2017-05-12 14:50:27 +0000

    增加UsrSet设置

--- SuperPOS/trunk/SuperPOS/UI/FrmTaAdminMain.cs
+++ SuperPOS/trunk/SuperPOS/UI/FrmTaAdminMain.cs
@@ -15,9 +15,21 @@ namespace SuperPOS.UI.TA
 {
     public partial class FrmTaAdminMain : DevExpress.XtraEditors.XtraForm
     {
+        //用户ID
+        public int usrID = 0;
+        //用户姓名
+        public string usrName = "";
+
         public FrmTaAdminMain()
         {
             InitializeComponent();
+        }
+
+        public FrmTaAdminMain(int uID, string sName)
+        {
+            InitializeComponent();
+            usrID = uID;
+            usrName = sName;
         }
 
         private void FrmTaAdminMain_Load(object sender, EventArgs e)
@@ -45,6 +57,23 @@ namespace SuperPOS.UI.TA
         private void SysExit_LinkClicked(object sender, DevExpress.XtraNavBar.NavBarLinkEventArgs e)
         {
             this.Close();
+        }
+
+        private void UsrSet_LinkClicked(object sender, DevExpress.XtraNavBar.NavBarLinkEventArgs e)
+        {
+            XtraTabPage page = new XtraTabPage();
+            page.Name = "UsrSet"; //窗口名字
+            page.Text = "User Setting";//窗口标题
+            if (!xtraTabControl1.TabPages.Any(s => s.Text.Equals(page.Text)))
+            {
+
+                FrmUsrSet frmUsrSet = new FrmUsrSet(usrID, usrName);
+                frmUsrSet.TopLevel = false;
+                frmUsrSet.Show();
+                page.Controls.Add(frmUsrSet);
+                this.xtraTabControl1.TabPages.Add(page);
+            }
+            xtraTabControl1.SelectedTabPage = xtraTabControl1.TabPages.FirstOrDefault(s => s.Text.Equals(page.Text));
         }
     }
 }
