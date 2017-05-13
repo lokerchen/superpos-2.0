5c173c4184f276be476601620f717b85333de976
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2017-05-13 08:22:23 +0000

    优化菜单点击优化

--- SuperPOS/trunk/SuperPOS/UI/FrmTaAdminMain.cs
+++ SuperPOS/trunk/SuperPOS/UI/FrmTaAdminMain.cs
@@ -39,19 +39,20 @@ namespace SuperPOS.UI.TA
 
         private void ShiftCode_LinkClicked(object sender, DevExpress.XtraNavBar.NavBarLinkEventArgs e)
         {
-            XtraTabPage page = new XtraTabPage();
-            page.Name = "SysShiftCode"; //窗口名字
-            page.Text = "Shift Code";//窗口标题
-            if (!xtraTabControl1.TabPages.Any(s => s.Text.Equals(page.Text)))
-            {
-                
-                FrmShiftCode frmShiftCode = new FrmShiftCode();
-                frmShiftCode.TopLevel = false;
-                frmShiftCode.Show();
-                page.Controls.Add(frmShiftCode);
-                this.xtraTabControl1.TabPages.Add(page);
-            }
-            xtraTabControl1.SelectedTabPage = xtraTabControl1.TabPages.FirstOrDefault(s => s.Text.Equals(page.Text));
+            //XtraTabPage page = new XtraTabPage();
+            //page.Name = "SysShiftCode"; //窗口名字
+            //page.Text = "Shift Code";//窗口标题
+            //if (!xtraTabControl1.TabPages.Any(s => s.Text.Equals(page.Text)))
+            //{
+
+            //    FrmShiftCode frmShiftCode = new FrmShiftCode();
+            //    frmShiftCode.TopLevel = false;
+            //    frmShiftCode.Show();
+            //    page.Controls.Add(frmShiftCode);
+            //    this.xtraTabControl1.TabPages.Add(page);
+            //}
+            //xtraTabControl1.SelectedTabPage = xtraTabControl1.TabPages.FirstOrDefault(s => s.Text.Equals(page.Text));
+            ShowXtraTabPage("SysShiftCode", "Shift Code", new FrmShiftCode(usrID, usrName));
         }
 
         private void SysExit_LinkClicked(object sender, DevExpress.XtraNavBar.NavBarLinkEventArgs e)
@@ -59,55 +60,89 @@ namespace SuperPOS.UI.TA
             this.Close();
         }
 
+        #region User Setting
         private void UsrSet_LinkClicked(object sender, DevExpress.XtraNavBar.NavBarLinkEventArgs e)
         {
-            XtraTabPage page = new XtraTabPage();
-            page.Name = "UsrSet"; //窗口名字
-            page.Text = "User Setting";//窗口标题
-            if (!xtraTabControl1.TabPages.Any(s => s.Text.Equals(page.Text)))
-            {
+            //XtraTabPage page = new XtraTabPage();
+            //page.Name = "UsrSet"; //窗口名字
+            //page.Text = "User Setting";//窗口标题
+            //if (!xtraTabControl1.TabPages.Any(s => s.Text.Equals(page.Text)))
+            //{
 
-                FrmUsrSet frmUsrSet = new FrmUsrSet(usrID, usrName);
-                frmUsrSet.TopLevel = false;
-                frmUsrSet.Show();
-                page.Controls.Add(frmUsrSet);
-                this.xtraTabControl1.TabPages.Add(page);
-            }
-            xtraTabControl1.SelectedTabPage = xtraTabControl1.TabPages.FirstOrDefault(s => s.Text.Equals(page.Text));
+            //    FrmUsrSet frmUsrSet = new FrmUsrSet(usrID, usrName);
+            //    frmUsrSet.TopLevel = false;
+            //    frmUsrSet.Show();
+            //    page.Controls.Add(frmUsrSet);
+            //    this.xtraTabControl1.TabPages.Add(page);
+            //}
+            //xtraTabControl1.SelectedTabPage = xtraTabControl1.TabPages.FirstOrDefault(s => s.Text.Equals(page.Text));
+            ShowXtraTabPage("UsrSet", "User Setting", new FrmUsrSet(usrID, usrName));
         }
+        #endregion
 
+        #region Computer Address
         private void CompAdd_LinkClicked(object sender, DevExpress.XtraNavBar.NavBarLinkEventArgs e)
         {
-            XtraTabPage page = new XtraTabPage();
-            page.Name = "CompAddr"; //窗口名字
-            page.Text = "Computer Address";//窗口标题
-            if (!xtraTabControl1.TabPages.Any(s => s.Text.Equals(page.Text)))
-            {
+            //XtraTabPage page = new XtraTabPage();
+            //page.Name = "CompAddr"; //窗口名字
+            //page.Text = "Computer Address";//窗口标题
+            //if (!xtraTabControl1.TabPages.Any(s => s.Text.Equals(page.Text)))
+            //{
 
-                FrmCompAddr frmUsrSet = new FrmCompAddr(usrID, usrName);
-                frmUsrSet.TopLevel = false;
-                frmUsrSet.Show();
-                page.Controls.Add(frmUsrSet);
-                this.xtraTabControl1.TabPages.Add(page);
-            }
-            xtraTabControl1.SelectedTabPage = xtraTabControl1.TabPages.FirstOrDefault(s => s.Text.Equals(page.Text));
+            //    FrmCompAddr frmUsrSet = new FrmCompAddr(usrID, usrName);
+            //    frmUsrSet.TopLevel = false;
+            //    frmUsrSet.Show();
+            //    page.Controls.Add(frmUsrSet);
+            //    this.xtraTabControl1.TabPages.Add(page);
+            //}
+            //xtraTabControl1.SelectedTabPage = xtraTabControl1.TabPages.FirstOrDefault(s => s.Text.Equals(page.Text));
+            ShowXtraTabPage("CompAddr", "Computer Address", new FrmCompAddr(usrID, usrName));
         }
+        #endregion
 
+        #region Shop Details
         private void ShopDetail_LinkClicked(object sender, DevExpress.XtraNavBar.NavBarLinkEventArgs e)
         {
+            //XtraTabPage page = new XtraTabPage();
+            //page.Name = "ShopDetail"; //窗口名字
+            //page.Text = "Shop Details";//窗口标题
+            //if (!xtraTabControl1.TabPages.Any(s => s.Text.Equals(page.Text)))
+            //{
+
+            //    FrmShopDetail frmShopDetail = new FrmShopDetail(usrID, usrName);
+            //    frmShopDetail.TopLevel = false;
+            //    frmShopDetail.Show();
+            //    page.Controls.Add(frmShopDetail);
+            //    this.xtraTabControl1.TabPages.Add(page);
+            //}
+            //xtraTabControl1.SelectedTabPage = xtraTabControl1.TabPages.FirstOrDefault(s => s.Text.Equals(page.Text));
+            ShowXtraTabPage("ShopDetail", "Shop Details", new FrmShopDetail(usrID, usrName));
+        }
+        #endregion
+
+        #region 显示XtraTabPage
+        /// <summary>
+        /// 显示XtraTabPage
+        /// </summary>
+        /// <param name="pageName">窗口名字</param>
+        /// <param name="pageText">窗口标题</param>
+        /// <param name="frmObject">窗口实例</param>
+        private void ShowXtraTabPage(string pageName, string pageText, Form frmObject)
+        {
             XtraTabPage page = new XtraTabPage();
-            page.Name = "ShopDetail"; //窗口名字
-            page.Text = "Shop Details";//窗口标题
+            page.Name = pageName; //窗口名字
+            page.Text = pageText;//窗口标题
             if (!xtraTabControl1.TabPages.Any(s => s.Text.Equals(page.Text)))
             {
 
-                FrmShopDetail frmShopDetail = new FrmShopDetail(usrID, usrName);
-                frmShopDetail.TopLevel = false;
-                frmShopDetail.Show();
-                page.Controls.Add(frmShopDetail);
+                Form frm = frmObject;
+                frm.TopLevel = false;
+                frm.Show();
+                page.Controls.Add(frm);
                 this.xtraTabControl1.TabPages.Add(page);
             }
             xtraTabControl1.SelectedTabPage = xtraTabControl1.TabPages.FirstOrDefault(s => s.Text.Equals(page.Text));
         }
+        #endregion
     }
 }
