39c6b53a871e40d4d6ac719d7e4a8dfb40383b68
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2017-05-17 17:32:57 +0000

    增加Takeaway点菜窗口

---
+++ SuperPOS/trunk/SuperPOS/UI/TA/FrmTaMain.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace SuperPOS.UI.TA
+{
+    public partial class FrmTaMain : DevExpress.XtraEditors.XtraForm
+    {
+        //操作用户ID
+        private int usrID;
+
+        //MenuItem按钮
+        private SimpleButton[] btnMi = new SimpleButton[32];
+        //ManuCate按钮
+        private SimpleButton[] btnMc = new SimpleButton[12];
+
+        public FrmTaMain()
+        {
+            InitializeComponent();
+        }
+
+        public FrmTaMain(int id)
+        {
+            InitializeComponent();
+            usrID = id;
+        }
+
+        #region 显示行号
+        private void treeListOrder_CustomDrawNodeIndicator(object sender, DevExpress.XtraTreeList.CustomDrawNodeIndicatorEventArgs e)
+        {
+            DevExpress.XtraTreeList.TreeList tmpTree = sender as DevExpress.XtraTreeList.TreeList;
+            DevExpress.Utils.Drawing.IndicatorObjectInfoArgs args = e.ObjectArgs as DevExpress.Utils.Drawing.IndicatorObjectInfoArgs;
+            if (args != null)
+            {
+                int rowNum = tmpTree.GetVisibleIndexByNode(e.Node) + 1;
+                this.treeListOrder.IndicatorWidth = rowNum.ToString().Length * 10 + 10;
+                args.DisplayText = rowNum.ToString();
+            }
+        }
+        #endregion
+
+        #region 设置MenuItem按钮
+        /// <summary>
+        /// 设置MenuItem按钮
+        /// </summary>
+        private void SetMiBtn()
+        {
+            btnMi[0] = btnMi0;
+            btnMi[1] = btnMi1;
+            btnMi[2] = btnMi2;
+            btnMi[3] = btnMi3;
+            btnMi[4] = btnMi4;
+            btnMi[5] = btnMi5;
+            btnMi[6] = btnMi6;
+            btnMi[7] = btnMi7;
+            btnMi[8] = btnMi8;
+            btnMi[9] = btnMi9;
+            btnMi[10] = btnMi10;
+            btnMi[11] = btnMi11;
+            btnMi[12] = btnMi12;
+            btnMi[13] = btnMi13;
+            btnMi[14] = btnMi14;
+            btnMi[15] = btnMi15;
+            btnMi[16] = btnMi16;
+            btnMi[17] = btnMi17;
+            btnMi[18] = btnMi18;
+            btnMi[19] = btnMi19;
+            btnMi[20] = btnMi20;
+            btnMi[21] = btnMi21;
+            btnMi[22] = btnMi22;
+            btnMi[23] = btnMi23;
+            btnMi[24] = btnMi24;
+            btnMi[25] = btnMi25;
+            btnMi[26] = btnMi26;
+            btnMi[27] = btnMi27;
+            btnMi[28] = btnMi28;
+            btnMi[29] = btnMi29;
+            btnMi[30] = btnMi30;
+            btnMi[31] = btnMi31;
+
+            for (int i = 0; i < 32; i++)
+            {
+                btnMi[i].Click += btnMi_Click;
+            }
+        }
+        #endregion
+
+        #region MenuItem 点击
+        /// <summary>
+        /// MenuItem 点击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnMi_Click(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion
+
+        #region 设置MenuCate按钮
+        /// <summary>
+        /// 设置MenuCate按钮
+        /// </summary>
+        private void SetMcBtn()
+        {
+            btnMc[0] = btnMc0;
+            btnMc[1] = btnMc1;
+            btnMc[2] = btnMc2;
+            btnMc[3] = btnMc3;
+            btnMc[4] = btnMc4;
+            btnMc[5] = btnMc5;
+            btnMc[6] = btnMc6;
+            btnMc[7] = btnMc7;
+            btnMc[8] = btnMc8;
+            btnMc[9] = btnMc9;
+            btnMc[10] = btnMc10;
+            btnMc[11] = btnMc11;
+
+            for (int i = 0; i < 12; i++)
+            {
+                btnMc[i].Click += btnMc_Click;
+            }
+        }
+        #endregion
+
+        #region MenuCate 点击
+        /// <summary>
+        /// MenuCate 点击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnMc_Click(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion
+
+        private void btnSaveOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
