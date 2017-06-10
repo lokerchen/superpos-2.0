173ef1f16cd2ac464df9d3fcdcc7cd2949544310
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2017-06-10 14:22:11 +0000

    切换Menu Set选择

---
+++ SuperPOS/trunk/SuperPOS/UI/TA/FrmTaMenuSelect.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SuperPOS.UI.TA
+{
+    partial class FrmTaMenuSelect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.btnExit = new DevExpress.XtraEditors.SimpleButton();
+            this.btnMs3 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnMs2 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnMs1 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnMs0 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnMs4 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.btnMs4);
+            this.panelControl1.Controls.Add(this.btnExit);
+            this.panelControl1.Controls.Add(this.btnMs3);
+            this.panelControl1.Controls.Add(this.btnMs2);
+            this.panelControl1.Controls.Add(this.btnMs1);
+            this.panelControl1.Controls.Add(this.btnMs0);
+            this.panelControl1.Location = new System.Drawing.Point(4, 5);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(158, 384);
+            this.panelControl1.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Appearance.Options.UseFont = true;
+            this.btnExit.Location = new System.Drawing.Point(5, 320);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(148, 57);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnMs3
+            // 
+            this.btnMs3.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMs3.Appearance.Options.UseFont = true;
+            this.btnMs3.Location = new System.Drawing.Point(5, 194);
+            this.btnMs3.Name = "btnMs3";
+            this.btnMs3.Size = new System.Drawing.Size(148, 57);
+            this.btnMs3.TabIndex = 3;
+            this.btnMs3.Text = "4";
+            // 
+            // btnMs2
+            // 
+            this.btnMs2.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMs2.Appearance.Options.UseFont = true;
+            this.btnMs2.Location = new System.Drawing.Point(5, 131);
+            this.btnMs2.Name = "btnMs2";
+            this.btnMs2.Size = new System.Drawing.Size(148, 57);
+            this.btnMs2.TabIndex = 2;
+            this.btnMs2.Text = "3";
+            // 
+            // btnMs1
+            // 
+            this.btnMs1.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMs1.Appearance.Options.UseFont = true;
+            this.btnMs1.Location = new System.Drawing.Point(5, 68);
+            this.btnMs1.Name = "btnMs1";
+            this.btnMs1.Size = new System.Drawing.Size(148, 57);
+            this.btnMs1.TabIndex = 1;
+            this.btnMs1.Text = "2";
+            // 
+            // btnMs0
+            // 
+            this.btnMs0.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMs0.Appearance.Options.UseFont = true;
+            this.btnMs0.Location = new System.Drawing.Point(5, 5);
+            this.btnMs0.Name = "btnMs0";
+            this.btnMs0.Size = new System.Drawing.Size(148, 57);
+            this.btnMs0.TabIndex = 0;
+            this.btnMs0.Text = "1";
+            // 
+            // btnMs4
+            // 
+            this.btnMs4.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMs4.Appearance.Options.UseFont = true;
+            this.btnMs4.Location = new System.Drawing.Point(5, 257);
+            this.btnMs4.Name = "btnMs4";
+            this.btnMs4.Size = new System.Drawing.Size(148, 57);
+            this.btnMs4.TabIndex = 5;
+            this.btnMs4.Text = "5";
+            // 
+            // FrmTaMenuSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(165, 396);
+            this.Controls.Add(this.panelControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmTaMenuSelect";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FrmTaMenuSelect";
+            this.Load += new System.EventHandler(this.FrmTaMenuSelect_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.SimpleButton btnExit;
+        private DevExpress.XtraEditors.SimpleButton btnMs3;
+        private DevExpress.XtraEditors.SimpleButton btnMs2;
+        private DevExpress.XtraEditors.SimpleButton btnMs1;
+        private DevExpress.XtraEditors.SimpleButton btnMs0;
+        private DevExpress.XtraEditors.SimpleButton btnMs4;
+    }
+}
---
+++ SuperPOS/trunk/SuperPOS/UI/TA/FrmTaMenuSelect.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using SuperPOS.Common;
+using SuperPOS.Domain.Entities;
+
+namespace SuperPOS.UI.TA
+{
+    public partial class FrmTaMenuSelect : DevExpress.XtraEditors.XtraForm
+    {
+        SimpleButton[] btnMenuSet = new SimpleButton[5];
+
+        public int msId = 0;
+
+        public int MenuSetId
+        {
+            get { return msId; }
+            set { MenuSetId = value; }
+        }
+
+        public FrmTaMenuSelect()
+        {
+            InitializeComponent();
+        }
+
+        #region 改码类型按钮
+        private void SetMsBtn()
+        {
+            btnMenuSet[0] = btnMs0;
+            btnMenuSet[1] = btnMs1;
+            btnMenuSet[2] = btnMs2;
+            btnMenuSet[3] = btnMs3;
+            btnMenuSet[4] = btnMs4;
+
+            for (int i = 0; i < 5; i++)
+            {
+                btnMenuSet[i].Click += btnMs_Click;
+            }
+
+            new SystemData().GetTaMenuSet();
+
+            btnMenuSet[0].Text = "ALL";
+
+            int j = 1;
+            foreach (var ms in CommonData.TaMenuSet)
+            {
+                btnMenuSet[j].Text = ms.MSEngName;
+                j++;
+            }
+
+            for (int i = j; i < 5; i++)
+            {
+                btnMenuSet[i].Visible = false;
+            }
+        }
+
+        private void btnMs_Click(object sender, EventArgs e)
+        {
+            SimpleButton btn = sender as SimpleButton;
+
+            if (btn.Text.Equals("ALL"))
+                msId = 0;
+            else
+            {
+                var lstMs = CommonData.TaMenuSet.Where(s => s.MSEngName.Equals(btn.Text));
+
+                if (lstMs.Any())
+                {
+                    msId = lstMs.FirstOrDefault().ID;
+                }
+            }
+
+            this.DialogResult = DialogResult.OK;
+            Hide();
+
+        }
+        #endregion
+
+        private void FrmTaMenuSelect_Load(object sender, EventArgs e)
+        {
+            SetMsBtn();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+    }
+}
