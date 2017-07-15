7cb59c5ed2320586a73f3bd767f9e05092f70029
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2017-07-15 08:05:17 +0000

    增加Pending Orders处理

---
+++ SuperPOS/trunk/SuperPOS/UI/TA/FrmTaPendOrder.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace SuperPOS.UI.TA
+{
+    partial class FrmTaPendOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.gridControlTaPendOrder = new DevExpress.XtraGrid.GridControl();
+            this.gvTaPendOrder = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.ID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.CheckCode = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.PayOrderType = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.TotalAmount = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.IsPaid = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.btnPrtKit = new DevExpress.XtraEditors.SimpleButton();
+            this.btnPrtBill = new DevExpress.XtraEditors.SimpleButton();
+            this.btnPrtReceipt = new DevExpress.XtraEditors.SimpleButton();
+            this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.btnDelivery = new DevExpress.XtraEditors.SimpleButton();
+            this.btnCollection = new DevExpress.XtraEditors.SimpleButton();
+            this.btnShop = new DevExpress.XtraEditors.SimpleButton();
+            this.btnAll = new DevExpress.XtraEditors.SimpleButton();
+            this.btnPay = new DevExpress.XtraEditors.SimpleButton();
+            this.btnExit = new DevExpress.XtraEditors.SimpleButton();
+            this.panelControl3 = new DevExpress.XtraEditors.PanelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlTaPendOrder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvTaPendOrder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+            this.panelControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).BeginInit();
+            this.panelControl3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.gridControlTaPendOrder);
+            this.panelControl1.Location = new System.Drawing.Point(12, 12);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(752, 559);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // gridControlTaPendOrder
+            // 
+            this.gridControlTaPendOrder.Location = new System.Drawing.Point(5, 5);
+            this.gridControlTaPendOrder.MainView = this.gvTaPendOrder;
+            this.gridControlTaPendOrder.Name = "gridControlTaPendOrder";
+            this.gridControlTaPendOrder.Size = new System.Drawing.Size(742, 549);
+            this.gridControlTaPendOrder.TabIndex = 3;
+            this.gridControlTaPendOrder.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvTaPendOrder});
+            // 
+            // gvTaPendOrder
+            // 
+            this.gvTaPendOrder.Appearance.EvenRow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.gvTaPendOrder.Appearance.EvenRow.Options.UseBackColor = true;
+            this.gvTaPendOrder.Appearance.HeaderPanel.Options.UseTextOptions = true;
+            this.gvTaPendOrder.Appearance.HeaderPanel.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.gvTaPendOrder.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.ID,
+            this.CheckCode,
+            this.PayOrderType,
+            this.TotalAmount,
+            this.IsPaid});
+            this.gvTaPendOrder.GridControl = this.gridControlTaPendOrder;
+            this.gvTaPendOrder.IndicatorWidth = 50;
+            this.gvTaPendOrder.Name = "gvTaPendOrder";
+            this.gvTaPendOrder.OptionsBehavior.Editable = false;
+            this.gvTaPendOrder.OptionsMenu.EnableColumnMenu = false;
+            this.gvTaPendOrder.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gvTaPendOrder.OptionsView.EnableAppearanceEvenRow = true;
+            this.gvTaPendOrder.OptionsView.EnableAppearanceOddRow = true;
+            this.gvTaPendOrder.OptionsView.ShowGroupPanel = false;
+            this.gvTaPendOrder.CustomDrawRowIndicator += new DevExpress.XtraGrid.Views.Grid.RowIndicatorCustomDrawEventHandler(this.gvTaPendOrder_CustomDrawRowIndicator);
+            this.gvTaPendOrder.FocusedRowChanged += new DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventHandler(this.gvTaPendOrder_FocusedRowChanged);
+            // 
+            // ID
+            // 
+            this.ID.AppearanceCell.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID.AppearanceCell.Options.UseFont = true;
+            this.ID.Caption = "ID";
+            this.ID.FieldName = "ID";
+            this.ID.Name = "ID";
+            // 
+            // CheckCode
+            // 
+            this.CheckCode.AppearanceCell.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckCode.AppearanceCell.Options.UseFont = true;
+            this.CheckCode.Caption = "Check Code";
+            this.CheckCode.FieldName = "CheckCode";
+            this.CheckCode.Name = "CheckCode";
+            this.CheckCode.Visible = true;
+            this.CheckCode.VisibleIndex = 0;
+            // 
+            // PayOrderType
+            // 
+            this.PayOrderType.AppearanceCell.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PayOrderType.AppearanceCell.Options.UseFont = true;
+            this.PayOrderType.Caption = "Order Type";
+            this.PayOrderType.FieldName = "PayOrderType";
+            this.PayOrderType.Name = "PayOrderType";
+            this.PayOrderType.Visible = true;
+            this.PayOrderType.VisibleIndex = 1;
+            // 
+            // TotalAmount
+            // 
+            this.TotalAmount.AppearanceCell.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalAmount.AppearanceCell.Options.UseFont = true;
+            this.TotalAmount.Caption = "Total Amount";
+            this.TotalAmount.FieldName = "TotalAmount";
+            this.TotalAmount.Name = "TotalAmount";
+            this.TotalAmount.Visible = true;
+            this.TotalAmount.VisibleIndex = 2;
+            // 
+            // IsPaid
+            // 
+            this.IsPaid.AppearanceCell.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsPaid.AppearanceCell.Options.UseFont = true;
+            this.IsPaid.Caption = "Is Paid";
+            this.IsPaid.FieldName = "IsPaid";
+            this.IsPaid.Name = "IsPaid";
+            this.IsPaid.Visible = true;
+            this.IsPaid.VisibleIndex = 3;
+            // 
+            // btnPrtKit
+            // 
+            this.btnPrtKit.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btnPrtKit.Appearance.Options.UseFont = true;
+            this.btnPrtKit.Location = new System.Drawing.Point(5, 165);
+            this.btnPrtKit.Name = "btnPrtKit";
+            this.btnPrtKit.Size = new System.Drawing.Size(184, 48);
+            this.btnPrtKit.TabIndex = 43;
+            this.btnPrtKit.Text = "Print Kitchen";
+            this.btnPrtKit.Click += new System.EventHandler(this.btnPrtKit_Click);
+            // 
+            // btnPrtBill
+            // 
+            this.btnPrtBill.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btnPrtBill.Appearance.Options.UseFont = true;
+            this.btnPrtBill.Location = new System.Drawing.Point(5, 111);
+            this.btnPrtBill.Name = "btnPrtBill";
+            this.btnPrtBill.Size = new System.Drawing.Size(184, 48);
+            this.btnPrtBill.TabIndex = 42;
+            this.btnPrtBill.Text = "Print Bill";
+            this.btnPrtBill.Click += new System.EventHandler(this.btnPrtBill_Click);
+            // 
+            // btnPrtReceipt
+            // 
+            this.btnPrtReceipt.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btnPrtReceipt.Appearance.Options.UseFont = true;
+            this.btnPrtReceipt.Location = new System.Drawing.Point(5, 57);
+            this.btnPrtReceipt.Name = "btnPrtReceipt";
+            this.btnPrtReceipt.Size = new System.Drawing.Size(184, 48);
+            this.btnPrtReceipt.TabIndex = 41;
+            this.btnPrtReceipt.Text = "Print Receipt";
+            this.btnPrtReceipt.Click += new System.EventHandler(this.btnPrtReceipt_Click);
+            // 
+            // panelControl2
+            // 
+            this.panelControl2.Controls.Add(this.btnPrtKit);
+            this.panelControl2.Controls.Add(this.btnPrtReceipt);
+            this.panelControl2.Controls.Add(this.btnPrtBill);
+            this.panelControl2.Location = new System.Drawing.Point(771, 12);
+            this.panelControl2.Name = "panelControl2";
+            this.panelControl2.Size = new System.Drawing.Size(200, 223);
+            this.panelControl2.TabIndex = 44;
+            // 
+            // btnDelivery
+            // 
+            this.btnDelivery.Appearance.Font = new System.Drawing.Font("Tahoma", 14F);
+            this.btnDelivery.Appearance.Options.UseFont = true;
+            this.btnDelivery.Location = new System.Drawing.Point(4, 5);
+            this.btnDelivery.Name = "btnDelivery";
+            this.btnDelivery.Size = new System.Drawing.Size(186, 48);
+            this.btnDelivery.TabIndex = 44;
+            this.btnDelivery.Text = "DELIVERY";
+            this.btnDelivery.Click += new System.EventHandler(this.btnDelivery_Click);
+            // 
+            // btnCollection
+            // 
+            this.btnCollection.Appearance.Font = new System.Drawing.Font("Tahoma", 14F);
+            this.btnCollection.Appearance.Options.UseFont = true;
+            this.btnCollection.Location = new System.Drawing.Point(4, 59);
+            this.btnCollection.Name = "btnCollection";
+            this.btnCollection.Size = new System.Drawing.Size(186, 48);
+            this.btnCollection.TabIndex = 45;
+            this.btnCollection.Text = "COLLECTION";
+            this.btnCollection.Click += new System.EventHandler(this.btnCollection_Click);
+            // 
+            // btnShop
+            // 
+            this.btnShop.Appearance.Font = new System.Drawing.Font("Tahoma", 14F);
+            this.btnShop.Appearance.Options.UseFont = true;
+            this.btnShop.Location = new System.Drawing.Point(4, 113);
+            this.btnShop.Name = "btnShop";
+            this.btnShop.Size = new System.Drawing.Size(186, 48);
+            this.btnShop.TabIndex = 46;
+            this.btnShop.Text = "SHOP";
+            this.btnShop.Click += new System.EventHandler(this.btnShop_Click);
+            // 
+            // btnAll
+            // 
+            this.btnAll.Appearance.Font = new System.Drawing.Font("Tahoma", 14F);
+            this.btnAll.Appearance.Options.UseFont = true;
+            this.btnAll.Location = new System.Drawing.Point(5, 167);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(185, 48);
+            this.btnAll.TabIndex = 47;
+            this.btnAll.Text = "All";
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
+            // btnPay
+            // 
+            this.btnPay.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btnPay.Appearance.Options.UseFont = true;
+            this.btnPay.Location = new System.Drawing.Point(771, 469);
+            this.btnPay.Name = "btnPay";
+            this.btnPay.Size = new System.Drawing.Size(189, 48);
+            this.btnPay.TabIndex = 44;
+            this.btnPay.Text = "PAY";
+            this.btnPay.Click += new System.EventHandler(this.btnPay_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Appearance.Font = new System.Drawing.Font("Tahoma", 14F);
+            this.btnExit.Appearance.Options.UseFont = true;
+            this.btnExit.Location = new System.Drawing.Point(771, 523);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(184, 48);
+            this.btnExit.TabIndex = 48;
+            this.btnExit.Text = "Exit";
+            // 
+            // panelControl3
+            // 
+            this.panelControl3.Controls.Add(this.btnAll);
+            this.panelControl3.Controls.Add(this.btnDelivery);
+            this.panelControl3.Controls.Add(this.btnCollection);
+            this.panelControl3.Controls.Add(this.btnShop);
+            this.panelControl3.Location = new System.Drawing.Point(770, 241);
+            this.panelControl3.Name = "panelControl3";
+            this.panelControl3.Size = new System.Drawing.Size(201, 221);
+            this.panelControl3.TabIndex = 49;
+            // 
+            // FrmTaPendOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 583);
+            this.Controls.Add(this.btnPay);
+            this.Controls.Add(this.panelControl3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.panelControl2);
+            this.Controls.Add(this.panelControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmTaPendOrder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FrmTaPendOrder";
+            this.Load += new System.EventHandler(this.FrmTaPendOrder_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlTaPendOrder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvTaPendOrder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+            this.panelControl2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).EndInit();
+            this.panelControl3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraGrid.GridControl gridControlTaPendOrder;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvTaPendOrder;
+        private DevExpress.XtraGrid.Columns.GridColumn ID;
+        private DevExpress.XtraGrid.Columns.GridColumn CheckCode;
+        private DevExpress.XtraGrid.Columns.GridColumn PayOrderType;
+        private DevExpress.XtraGrid.Columns.GridColumn TotalAmount;
+        private DevExpress.XtraGrid.Columns.GridColumn IsPaid;
+        private DevExpress.XtraEditors.SimpleButton btnPrtKit;
+        private DevExpress.XtraEditors.SimpleButton btnPrtBill;
+        private DevExpress.XtraEditors.SimpleButton btnPrtReceipt;
+        private DevExpress.XtraEditors.PanelControl panelControl2;
+        private DevExpress.XtraEditors.SimpleButton btnPay;
+        private DevExpress.XtraEditors.SimpleButton btnDelivery;
+        private DevExpress.XtraEditors.SimpleButton btnCollection;
+        private DevExpress.XtraEditors.SimpleButton btnShop;
+        private DevExpress.XtraEditors.SimpleButton btnAll;
+        private DevExpress.XtraEditors.SimpleButton btnExit;
+        private DevExpress.XtraEditors.PanelControl panelControl3;
+    }
+}
---
+++ SuperPOS/trunk/SuperPOS/UI/TA/FrmTaPendOrder.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using SuperPOS.Common;
+using SuperPOS.Domain.Entities;
+using SuperPOS.Print;
+
+namespace SuperPOS.UI.TA
+{
+    public partial class FrmTaPendOrder : DevExpress.XtraEditors.XtraForm
+    {
+        //用户ID
+        private int usrID;
+
+        //账单ID
+        private int checkID;
+        //账单号
+        private string checkCode;
+        //账单类型
+        private string checkOrderType;
+        //账单总价
+        private string checkTotalAmount;
+        //已付款
+        private string checkPaid;
+        //是否已付
+        private string checkIsPaid;
+        //下单用户
+        private string checkUsrName;
+        //来电ID
+        private string checkCustID;
+        //司机ID
+        private int checkDriverID;
+
+        public FrmTaPendOrder()
+        {
+            InitializeComponent();
+        }
+
+        public FrmTaPendOrder(int uID)
+        {
+            InitializeComponent();
+            usrID = uID;
+        }
+
+        private void GetBindData(string orderType)
+        {
+            var lstDb = from check in CommonData.TaCheckOrder
+                join cust in CommonData.TaCustomer
+                    on check.CustomerID equals cust.ID.ToString()
+                join user in CommonData.UsrBase
+                    on check.StaffID equals user.ID
+                select new
+                {
+                    ID = check.ID,
+                    CheckCode = check.CheckCode,
+                    PayOrderType = check.PayOrderType,
+                    IsPaid = check.IsPaid,
+                    TotalAmount = check.TotalAmount,
+                    StaffName = user.UsrName,
+                    Paid = check.Paid,
+                    CustID = cust.ID,
+                    DriverID = check.DriverID
+                };
+
+            gridControlTaPendOrder.DataSource = !string.IsNullOrEmpty(orderType) ? lstDb.Where(s => s.PayOrderType.Equals(orderType)).ToList() : lstDb.ToList();
+            gvTaPendOrder.FocusedRowHandle = gvTaPendOrder.RowCount - 1;
+        }
+
+        private void FrmTaPendOrder_Load(object sender, EventArgs e)
+        {
+            SystemData systemData = new SystemData();
+            systemData.GetTaCheckOrder();
+            systemData.GetTaCustomer();
+            systemData.GetUsrBase();
+
+            GetBindData("");
+        }
+
+        private void gvTaPendOrder_CustomDrawRowIndicator(object sender, DevExpress.XtraGrid.Views.Grid.RowIndicatorCustomDrawEventArgs e)
+        {
+            if (e.Info.IsRowIndicator && e.RowHandle > -1)
+            {
+                e.Info.DisplayText = (e.RowHandle + 1).ToString();
+            }
+        }
+
+        private void gvTaPendOrder_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            //账单ID
+            checkID = Convert.ToInt32(gvTaPendOrder.GetRowCellValue(gvTaPendOrder.FocusedRowHandle, "ID").ToString());
+            //账单号
+            checkCode = gvTaPendOrder.GetRowCellValue(gvTaPendOrder.FocusedRowHandle, "CheckCode").ToString();
+            //账单类型
+            checkOrderType = gvTaPendOrder.GetRowCellValue(gvTaPendOrder.FocusedRowHandle, "PayOrderType").ToString();
+            //账单总价
+            checkTotalAmount = gvTaPendOrder.GetRowCellValue(gvTaPendOrder.FocusedRowHandle, "TotalAmount").ToString();
+            //已付款
+            checkPaid = gvTaPendOrder.GetRowCellValue(gvTaPendOrder.FocusedRowHandle, "Paid").ToString();
+            //已付
+            checkIsPaid = gvTaPendOrder.GetRowCellValue(gvTaPendOrder.FocusedRowHandle, "IsPaid").ToString();
+            //下单用户
+            checkUsrName = gvTaPendOrder.GetRowCellValue(gvTaPendOrder.FocusedRowHandle, "StaffName").ToString();
+            //来电ID
+            checkCustID = gvTaPendOrder.GetRowCellValue(gvTaPendOrder.FocusedRowHandle, "CustID").ToString();
+            //司机ID
+            checkDriverID = Convert.ToInt32(gvTaPendOrder.GetRowCellValue(gvTaPendOrder.FocusedRowHandle, "DriverID").ToString());
+        }
+
+        private void btnPay_Click(object sender, EventArgs e)
+        {
+            FrmTaPayment frmTaPayment = new FrmTaPayment(usrID, checkCode, checkOrderType, checkCustID, SetPrtInfo());
+
+            if (frmTaPayment.ShowDialog() == DialogResult.OK)
+            {
+                if (frmTaPayment.returnPaid) GetBindData("ALL");
+            }
+        }
+
+        private void btnDelivery_Click(object sender, EventArgs e)
+        {
+            GetBindData(PubComm.ORDER_TYPE_DELIVERY);
+        }
+
+        private void btnCollection_Click(object sender, EventArgs e)
+        {
+            GetBindData(PubComm.ORDER_TYPE_COLLECTION);
+        }
+
+        private void btnShop_Click(object sender, EventArgs e)
+        {
+            GetBindData(PubComm.ORDER_TYPE_SHOP);
+        }
+
+        private void btnAll_Click(object sender, EventArgs e)
+        {
+            GetBindData("");
+        }
+
+        #region 设置打印相关信息
+
+        private Hashtable SetPrtInfo()
+        {
+            Hashtable htDetail = new Hashtable();
+
+            new SystemData().GetUsrBase();
+
+            htDetail["Staff"] = CommonData.UsrBase.Any(s => s.ID == usrID) ? CommonData.UsrBase.FirstOrDefault(s => s.ID == usrID).UsrName : "";
+
+            var lstOi = CommonData.TaOrderItem.Where(s => s.CheckCode.Equals(checkCode));
+
+            htDetail["ItemQty"] = lstOi.Count(s => s.ItemType == 1);
+            htDetail["SubTotal"] = checkTotalAmount;
+            htDetail["Total"] = checkTotalAmount;
+            htDetail["PayType"] = checkOrderType;
+            htDetail["OrderNo"] = checkCode;
+
+            return htDetail;
+        }
+        #endregion
+
+        private void btnPrtReceipt_Click(object sender, EventArgs e)
+        {
+            Hashtable ht = SetPrtInfo();
+            ht["Tendered"] = checkPaid;
+
+            ht["Change"] = Convert.ToDecimal(checkTotalAmount) - Convert.ToDecimal(checkPaid) >= 0.0m
+                ? (Convert.ToDecimal(checkTotalAmount) - Convert.ToDecimal(checkPaid)).ToString()
+                : (Convert.ToDecimal(checkPaid) - Convert.ToDecimal(checkTotalAmount)).ToString();
+
+            new SystemData().GetTaOrderItem();
+            var lstOi = CommonData.TaOrderItem.Where(s => s.CheckCode.Equals(checkCode)).ToList();
+
+            #region VAT计算
+            if (CommonData.GenSet.Any())
+            {
+                ht["Rate1"] = CommonData.GenSet.FirstOrDefault().VATPer + @"%";
+
+                var lstVAT = from oi in CommonData.TaOrderItem.Where(s => s.CheckCode.Equals(checkID))
+                             join mi in CommonData.TaMenuItem on oi.ItemCode equals mi.MiDishCode
+                             where !string.IsNullOrEmpty(mi.MiRmk) && mi.MiRmk.Contains(@"Without VAT")
+                             select new
+                             {
+                                 itemTotalPrice = oi.ItemTotalPrice
+                             };
+
+                decimal dTotal = 0.00m;
+                decimal dVatTmp = 0.00m;
+                decimal dVat = 0.00m;
+
+                if (lstVAT.Any())
+                {
+                    dTotal = lstVAT.ToList().Sum(vat => Convert.ToDecimal(vat.itemTotalPrice));
+                    //交税
+                    dVatTmp = (Convert.ToDecimal(CommonData.GenSet.FirstOrDefault().VATPer) / 100) * dTotal;
+
+                    dVat = Math.Round(dVatTmp, 2, MidpointRounding.AwayFromZero);
+                }
+
+                ht["VAT-A"] = dVat.ToString();
+                //税前
+                ht["Net1"] = dTotal.ToString();
+                //总价
+                ht["Gross1"] = (dTotal - dVat).ToString();
+                ht["Rate2"] = "0.00%";
+                ht["Net2"] = (Convert.ToDecimal(checkTotalAmount) - dTotal).ToString();
+                ht["VAT-B"] = "0.00";
+                ht["Gross2"] = (Convert.ToDecimal(checkTotalAmount) - dTotal).ToString();
+            }
+            else
+            {
+                ht["Rate1"] = "0.00%";
+                ht["Net1"] = "0.00";
+                ht["VAT-A"] = "0.00";
+                ht["Gross1"] = "0.00";
+                ht["Rate2"] = "0.00%";
+                ht["Net2"] = "0.00";
+                ht["VAT-B"] = "0.00";
+                ht["Gross2"] = "0.00";
+            }
+            #endregion
+
+            PrtPrint.PrtReceipt(lstOi, ht);
+        }
+
+        private void btnPrtBill_Click(object sender, EventArgs e)
+        {
+            Hashtable ht = SetPrtInfo();
+            ht["Tendered"] = checkPaid;
+
+            ht["Change"] = Convert.ToDecimal(checkTotalAmount) - Convert.ToDecimal(checkPaid) >= 0.0m
+                ? (Convert.ToDecimal(checkTotalAmount) - Convert.ToDecimal(checkPaid)).ToString()
+                : (Convert.ToDecimal(checkPaid) - Convert.ToDecimal(checkTotalAmount)).ToString();
+
+            new SystemData().GetTaOrderItem();
+            var lstOi = CommonData.TaOrderItem.Where(s => s.CheckCode.Equals(checkCode)).ToList();
+
+            PrtPrint.PrtBillBilingual(lstOi, ht);
+        }
+
+        private void btnPrtKit_Click(object sender, EventArgs e)
+        {
+            Hashtable ht = SetPrtInfo();
+            ht["ChkNum"] = checkCode;
+
+            new SystemData().GetTaOrderItem();
+            var lstOi = CommonData.TaOrderItem.Where(s => s.CheckCode.Equals(checkCode)).ToList();
+
+            PrtPrint.PrtKitchen(lstOi, ht);
+        }
+    }
+}
