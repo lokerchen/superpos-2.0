a2d19a6a0025e4a869668b15550a3be37217f8e8
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2017-12-20 13:12:32 +0000

    增加依据屏幕自动缩放

--- SuperPOS/trunk/SuperPOS/UI/FrmDesktopArea.Designer.cs
+++ SuperPOS/trunk/SuperPOS/UI/FrmDesktopArea.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
-            this.btnDrawer = new DevExpress.XtraEditors.SimpleButton();
-            this.btnTakeaway = new DevExpress.XtraEditors.SimpleButton();
-            this.btnEatIn = new DevExpress.XtraEditors.SimpleButton();
-            this.btnQuickFood = new DevExpress.XtraEditors.SimpleButton();
-            this.btnCtlPanel = new DevExpress.XtraEditors.SimpleButton();
-            this.btnShowOrder = new DevExpress.XtraEditors.SimpleButton();
-            this.btnLogout = new DevExpress.XtraEditors.SimpleButton();
-            this.lblSession = new DevExpress.XtraEditors.LabelControl();
             this.lblUsrName = new DevExpress.XtraEditors.LabelControl();
+            this.lblSession = new DevExpress.XtraEditors.LabelControl();
+            this.btnQuickFood = new DevExpress.XtraEditors.SimpleButton();
+            this.btnEatIn = new DevExpress.XtraEditors.SimpleButton();
+            this.btnTakeaway = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLogout = new DevExpress.XtraEditors.SimpleButton();
+            this.btnShowOrder = new DevExpress.XtraEditors.SimpleButton();
+            this.btnCtlPanel = new DevExpress.XtraEditors.SimpleButton();
+            this.btnDrawer = new DevExpress.XtraEditors.SimpleButton();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
             this.panelControl1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -47,7 +47,6 @@
             // 
             // panelControl1
             // 
-            this.panelControl1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panelControl1.Controls.Add(this.lblUsrName);
             this.panelControl1.Controls.Add(this.lblSession);
             this.panelControl1.Controls.Add(this.btnQuickFood);
@@ -55,10 +54,68 @@
             this.panelControl1.Controls.Add(this.btnTakeaway);
             this.panelControl1.Controls.Add(this.labelControl1);
             this.panelControl1.Controls.Add(this.panel1);
-            this.panelControl1.Location = new System.Drawing.Point(4, 13);
+            this.panelControl1.Location = new System.Drawing.Point(4, 7);
             this.panelControl1.Name = "panelControl1";
             this.panelControl1.Size = new System.Drawing.Size(698, 559);
             this.panelControl1.TabIndex = 0;
+            // 
+            // lblUsrName
+            // 
+            this.lblUsrName.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsrName.Location = new System.Drawing.Point(45, 104);
+            this.lblUsrName.Name = "lblUsrName";
+            this.lblUsrName.Size = new System.Drawing.Size(101, 36);
+            this.lblUsrName.TabIndex = 11;
+            this.lblUsrName.Text = "STAFF8";
+            // 
+            // lblSession
+            // 
+            this.lblSession.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSession.Location = new System.Drawing.Point(45, 62);
+            this.lblSession.Name = "lblSession";
+            this.lblSession.Size = new System.Drawing.Size(108, 36);
+            this.lblSession.TabIndex = 10;
+            this.lblSession.Text = "DINNER";
+            // 
+            // btnQuickFood
+            // 
+            this.btnQuickFood.Appearance.Font = new System.Drawing.Font("Tahoma", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuickFood.Appearance.Options.UseFont = true;
+            this.btnQuickFood.Location = new System.Drawing.Point(284, 433);
+            this.btnQuickFood.Name = "btnQuickFood";
+            this.btnQuickFood.Size = new System.Drawing.Size(352, 109);
+            this.btnQuickFood.TabIndex = 4;
+            this.btnQuickFood.Text = "Quick Food";
+            // 
+            // btnEatIn
+            // 
+            this.btnEatIn.Appearance.Font = new System.Drawing.Font("Tahoma", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEatIn.Appearance.Options.UseFont = true;
+            this.btnEatIn.Location = new System.Drawing.Point(284, 297);
+            this.btnEatIn.Name = "btnEatIn";
+            this.btnEatIn.Size = new System.Drawing.Size(352, 109);
+            this.btnEatIn.TabIndex = 3;
+            this.btnEatIn.Text = "Eat In";
+            // 
+            // btnTakeaway
+            // 
+            this.btnTakeaway.Appearance.Font = new System.Drawing.Font("Tahoma", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTakeaway.Appearance.Options.UseFont = true;
+            this.btnTakeaway.Location = new System.Drawing.Point(284, 156);
+            this.btnTakeaway.Name = "btnTakeaway";
+            this.btnTakeaway.Size = new System.Drawing.Size(352, 109);
+            this.btnTakeaway.TabIndex = 2;
+            this.btnTakeaway.Text = "Takeaway";
+            this.btnTakeaway.Click += new System.EventHandler(this.btnTakeaway_Click);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Location = new System.Drawing.Point(268, 5);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(413, 84);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "Desktop Area";
             // 
             // panel1
             // 
@@ -71,55 +128,26 @@
             this.panel1.Size = new System.Drawing.Size(171, 394);
             this.panel1.TabIndex = 0;
             // 
-            // labelControl1
+            // btnLogout
             // 
-            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelControl1.Location = new System.Drawing.Point(268, 5);
-            this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(413, 84);
-            this.labelControl1.TabIndex = 1;
-            this.labelControl1.Text = "Desktop Area";
+            this.btnLogout.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Appearance.Options.UseFont = true;
+            this.btnLogout.Location = new System.Drawing.Point(8, 304);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(155, 78);
+            this.btnLogout.TabIndex = 9;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
-            // btnDrawer
+            // btnShowOrder
             // 
-            this.btnDrawer.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDrawer.Appearance.Options.UseFont = true;
-            this.btnDrawer.Location = new System.Drawing.Point(8, 12);
-            this.btnDrawer.Name = "btnDrawer";
-            this.btnDrawer.Size = new System.Drawing.Size(155, 75);
-            this.btnDrawer.TabIndex = 6;
-            this.btnDrawer.Text = "Cash Drawer";
-            // 
-            // btnTakeaway
-            // 
-            this.btnTakeaway.Appearance.Font = new System.Drawing.Font("Tahoma", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTakeaway.Appearance.Options.UseFont = true;
-            this.btnTakeaway.Location = new System.Drawing.Point(268, 156);
-            this.btnTakeaway.Name = "btnTakeaway";
-            this.btnTakeaway.Size = new System.Drawing.Size(352, 109);
-            this.btnTakeaway.TabIndex = 2;
-            this.btnTakeaway.Text = "Takeaway";
-            this.btnTakeaway.Click += new System.EventHandler(this.btnTakeaway_Click);
-            // 
-            // btnEatIn
-            // 
-            this.btnEatIn.Appearance.Font = new System.Drawing.Font("Tahoma", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEatIn.Appearance.Options.UseFont = true;
-            this.btnEatIn.Location = new System.Drawing.Point(268, 297);
-            this.btnEatIn.Name = "btnEatIn";
-            this.btnEatIn.Size = new System.Drawing.Size(352, 109);
-            this.btnEatIn.TabIndex = 3;
-            this.btnEatIn.Text = "Eat In";
-            // 
-            // btnQuickFood
-            // 
-            this.btnQuickFood.Appearance.Font = new System.Drawing.Font("Tahoma", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnQuickFood.Appearance.Options.UseFont = true;
-            this.btnQuickFood.Location = new System.Drawing.Point(268, 433);
-            this.btnQuickFood.Name = "btnQuickFood";
-            this.btnQuickFood.Size = new System.Drawing.Size(352, 109);
-            this.btnQuickFood.TabIndex = 4;
-            this.btnQuickFood.Text = "Quick Food";
+            this.btnShowOrder.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowOrder.Appearance.Options.UseFont = true;
+            this.btnShowOrder.Location = new System.Drawing.Point(8, 109);
+            this.btnShowOrder.Name = "btnShowOrder";
+            this.btnShowOrder.Size = new System.Drawing.Size(155, 78);
+            this.btnShowOrder.TabIndex = 8;
+            this.btnShowOrder.Text = "Show Order";
             // 
             // btnCtlPanel
             // 
@@ -132,56 +160,28 @@
             this.btnCtlPanel.Text = "Control Panel";
             this.btnCtlPanel.Click += new System.EventHandler(this.btnCtlPanel_Click);
             // 
-            // btnShowOrder
+            // btnDrawer
             // 
-            this.btnShowOrder.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnShowOrder.Appearance.Options.UseFont = true;
-            this.btnShowOrder.Location = new System.Drawing.Point(8, 109);
-            this.btnShowOrder.Name = "btnShowOrder";
-            this.btnShowOrder.Size = new System.Drawing.Size(155, 78);
-            this.btnShowOrder.TabIndex = 8;
-            this.btnShowOrder.Text = "Show Order";
-            // 
-            // btnLogout
-            // 
-            this.btnLogout.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogout.Appearance.Options.UseFont = true;
-            this.btnLogout.Location = new System.Drawing.Point(8, 304);
-            this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(155, 78);
-            this.btnLogout.TabIndex = 9;
-            this.btnLogout.Text = "Logout";
-            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
-            // 
-            // lblSession
-            // 
-            this.lblSession.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSession.Location = new System.Drawing.Point(37, 62);
-            this.lblSession.Name = "lblSession";
-            this.lblSession.Size = new System.Drawing.Size(108, 36);
-            this.lblSession.TabIndex = 10;
-            this.lblSession.Text = "DINNER";
-            // 
-            // lblUsrName
-            // 
-            this.lblUsrName.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsrName.Location = new System.Drawing.Point(37, 104);
-            this.lblUsrName.Name = "lblUsrName";
-            this.lblUsrName.Size = new System.Drawing.Size(101, 36);
-            this.lblUsrName.TabIndex = 11;
-            this.lblUsrName.Text = "STAFF8";
+            this.btnDrawer.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDrawer.Appearance.Options.UseFont = true;
+            this.btnDrawer.Location = new System.Drawing.Point(8, 12);
+            this.btnDrawer.Name = "btnDrawer";
+            this.btnDrawer.Size = new System.Drawing.Size(155, 75);
+            this.btnDrawer.TabIndex = 6;
+            this.btnDrawer.Text = "Cash Drawer";
             // 
             // FrmDesktopArea
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(708, 594);
+            this.ClientSize = new System.Drawing.Size(708, 573);
             this.Controls.Add(this.panelControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmDesktopArea";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.FrmDesktopArea_Load);
+            this.SizeChanged += new System.EventHandler(this.FrmDesktopArea_SizeChanged);
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
             this.panelControl1.ResumeLayout(false);
             this.panelControl1.PerformLayout();
