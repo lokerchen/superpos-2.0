2eb147174436b4fec51108fe1affe0ac81d3f0b6
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2018-01-25 06:27:43 +0000

    1.增加退出按钮 2.修改页面展示相关内容

--- SuperPOS/branch/SuperPOS/UI/FrmShow.Designer.cs
+++ SuperPOS/branch/SuperPOS/UI/FrmShow.Designer.cs
@@ -32,39 +32,42 @@
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.btnLogon = new DevExpress.XtraEditors.SimpleButton();
             this.btnRemote = new DevExpress.XtraEditors.SimpleButton();
-            this.btnOnline = new DevExpress.XtraEditors.SimpleButton();
             this.btnSoftUpgrade = new DevExpress.XtraEditors.SimpleButton();
             this.picImg = new System.Windows.Forms.PictureBox();
+            this.btnExit = new DevExpress.XtraEditors.SimpleButton();
             ((System.ComponentModel.ISupportInitialize)(this.picImg)).BeginInit();
             this.SuspendLayout();
             // 
             // labelControl2
             // 
             this.labelControl2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.labelControl2.Location = new System.Drawing.Point(138, 439);
+            this.labelControl2.Location = new System.Drawing.Point(227, 495);
             this.labelControl2.Name = "labelControl2";
             this.labelControl2.Size = new System.Drawing.Size(412, 18);
             this.labelControl2.TabIndex = 13;
-            this.labelControl2.Text = "Copyright @ 2017 All Right Reserved     Gwenwich (UK) Limited";
+            this.labelControl2.Text = "Copyright @ 2018 All Right Reserved     Gwenwich (UK) Limited";
             // 
             // labelControl1
             // 
             this.labelControl1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelControl1.Appearance.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelControl1.Location = new System.Drawing.Point(140, 12);
+            this.labelControl1.Location = new System.Drawing.Point(255, 12);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(410, 39);
+            this.labelControl1.Size = new System.Drawing.Size(397, 39);
             this.labelControl1.TabIndex = 12;
-            this.labelControl1.Text = "SuperPOS ePOS System V2.0";
+            this.labelControl1.Text = "FadaPOS ePOS System V1.0";
             // 
             // btnLogon
             // 
             this.btnLogon.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnLogon.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogon.Appearance.Options.UseFont = true;
-            this.btnLogon.Location = new System.Drawing.Point(413, 341);
+            this.btnLogon.Appearance.Options.UseTextOptions = true;
+            this.btnLogon.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
+            this.btnLogon.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap;
+            this.btnLogon.Location = new System.Drawing.Point(466, 351);
             this.btnLogon.Name = "btnLogon";
-            this.btnLogon.Size = new System.Drawing.Size(314, 71);
+            this.btnLogon.Size = new System.Drawing.Size(210, 110);
             this.btnLogon.TabIndex = 8;
             this.btnLogon.Text = "Logon to SuperPOS";
             this.btnLogon.Click += new System.EventHandler(this.btnLogon_Click);
@@ -74,53 +77,63 @@
             this.btnRemote.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnRemote.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRemote.Appearance.Options.UseFont = true;
-            this.btnRemote.Location = new System.Drawing.Point(413, 253);
+            this.btnRemote.Appearance.Options.UseTextOptions = true;
+            this.btnRemote.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
+            this.btnRemote.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap;
+            this.btnRemote.Location = new System.Drawing.Point(241, 351);
             this.btnRemote.Name = "btnRemote";
-            this.btnRemote.Size = new System.Drawing.Size(314, 71);
+            this.btnRemote.Size = new System.Drawing.Size(210, 110);
             this.btnRemote.TabIndex = 10;
             this.btnRemote.Text = "Remote Support";
-            // 
-            // btnOnline
-            // 
-            this.btnOnline.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnOnline.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOnline.Appearance.Options.UseFont = true;
-            this.btnOnline.Location = new System.Drawing.Point(413, 162);
-            this.btnOnline.Name = "btnOnline";
-            this.btnOnline.Size = new System.Drawing.Size(314, 71);
-            this.btnOnline.TabIndex = 9;
-            this.btnOnline.Text = "Milpo Online Takeaway NetShop";
             // 
             // btnSoftUpgrade
             // 
             this.btnSoftUpgrade.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnSoftUpgrade.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSoftUpgrade.Appearance.Options.UseFont = true;
-            this.btnSoftUpgrade.Location = new System.Drawing.Point(413, 71);
+            this.btnSoftUpgrade.Appearance.Options.UseTextOptions = true;
+            this.btnSoftUpgrade.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
+            this.btnSoftUpgrade.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap;
+            this.btnSoftUpgrade.Location = new System.Drawing.Point(9, 351);
             this.btnSoftUpgrade.Name = "btnSoftUpgrade";
-            this.btnSoftUpgrade.Size = new System.Drawing.Size(314, 71);
+            this.btnSoftUpgrade.Size = new System.Drawing.Size(210, 110);
             this.btnSoftUpgrade.TabIndex = 12;
             this.btnSoftUpgrade.Text = "Software Upgrade And Licence";
             // 
             // picImg
             // 
             this.picImg.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.picImg.Location = new System.Drawing.Point(8, 71);
+            this.picImg.Location = new System.Drawing.Point(286, 74);
             this.picImg.Name = "picImg";
-            this.picImg.Size = new System.Drawing.Size(370, 350);
+            this.picImg.Size = new System.Drawing.Size(338, 247);
             this.picImg.TabIndex = 7;
             this.picImg.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnExit.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Appearance.Options.UseFont = true;
+            this.btnExit.Appearance.Options.UseTextOptions = true;
+            this.btnExit.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
+            this.btnExit.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap;
+            this.btnExit.Location = new System.Drawing.Point(691, 351);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(210, 110);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "Exit";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // FrmShow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(743, 470);
+            this.ClientSize = new System.Drawing.Size(913, 537);
+            this.Controls.Add(this.btnExit);
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.btnLogon);
             this.Controls.Add(this.btnRemote);
-            this.Controls.Add(this.btnOnline);
             this.Controls.Add(this.btnSoftUpgrade);
             this.Controls.Add(this.picImg);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -140,8 +153,8 @@
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.SimpleButton btnLogon;
         private DevExpress.XtraEditors.SimpleButton btnRemote;
-        private DevExpress.XtraEditors.SimpleButton btnOnline;
         private DevExpress.XtraEditors.SimpleButton btnSoftUpgrade;
         private System.Windows.Forms.PictureBox picImg;
+        private DevExpress.XtraEditors.SimpleButton btnExit;
     }
 }
