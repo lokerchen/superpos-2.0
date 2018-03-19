52b13269018cd690ee2a89500ebfdf0613f5e080
superpos <superpos@81cb8fb1-5029-40e2-a703-34f33f8b8ffe>
2018-03-19 07:11:02 +0000

    修正UI

--- SuperPOS/branch/SuperPOS/UI/FrmShow.Designer.cs
+++ SuperPOS/branch/SuperPOS/UI/FrmShow.Designer.cs
@@ -28,8 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
-            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmShow));
             this.btnLogon = new DevExpress.XtraEditors.SimpleButton();
             this.btnRemote = new DevExpress.XtraEditors.SimpleButton();
             this.btnSoftUpgrade = new DevExpress.XtraEditors.SimpleButton();
@@ -38,34 +37,20 @@
             ((System.ComponentModel.ISupportInitialize)(this.picImg)).BeginInit();
             this.SuspendLayout();
             // 
-            // labelControl2
-            // 
-            this.labelControl2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.labelControl2.Location = new System.Drawing.Point(227, 495);
-            this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(412, 18);
-            this.labelControl2.TabIndex = 13;
-            this.labelControl2.Text = "Copyright @ 2018 All Right Reserved     Gwenwich (UK) Limited";
-            // 
-            // labelControl1
-            // 
-            this.labelControl1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.labelControl1.Appearance.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelControl1.Location = new System.Drawing.Point(255, 12);
-            this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(397, 39);
-            this.labelControl1.TabIndex = 12;
-            this.labelControl1.Text = "FadaPOS ePOS System V1.0";
-            // 
             // btnLogon
             // 
             this.btnLogon.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnLogon.Appearance.BackColor = System.Drawing.Color.Gold;
             this.btnLogon.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogon.Appearance.ForeColor = System.Drawing.Color.White;
+            this.btnLogon.Appearance.Options.UseBackColor = true;
             this.btnLogon.Appearance.Options.UseFont = true;
+            this.btnLogon.Appearance.Options.UseForeColor = true;
             this.btnLogon.Appearance.Options.UseTextOptions = true;
             this.btnLogon.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
             this.btnLogon.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap;
-            this.btnLogon.Location = new System.Drawing.Point(466, 351);
+            this.btnLogon.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.btnLogon.Location = new System.Drawing.Point(478, 217);
             this.btnLogon.Name = "btnLogon";
             this.btnLogon.Size = new System.Drawing.Size(210, 110);
             this.btnLogon.TabIndex = 8;
@@ -75,12 +60,17 @@
             // btnRemote
             // 
             this.btnRemote.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnRemote.Appearance.BackColor = System.Drawing.Color.Sienna;
             this.btnRemote.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemote.Appearance.ForeColor = System.Drawing.Color.White;
+            this.btnRemote.Appearance.Options.UseBackColor = true;
             this.btnRemote.Appearance.Options.UseFont = true;
+            this.btnRemote.Appearance.Options.UseForeColor = true;
             this.btnRemote.Appearance.Options.UseTextOptions = true;
             this.btnRemote.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
             this.btnRemote.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap;
-            this.btnRemote.Location = new System.Drawing.Point(241, 351);
+            this.btnRemote.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.btnRemote.Location = new System.Drawing.Point(253, 217);
             this.btnRemote.Name = "btnRemote";
             this.btnRemote.Size = new System.Drawing.Size(210, 110);
             this.btnRemote.TabIndex = 10;
@@ -89,12 +79,17 @@
             // btnSoftUpgrade
             // 
             this.btnSoftUpgrade.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSoftUpgrade.Appearance.BackColor = System.Drawing.Color.Lime;
             this.btnSoftUpgrade.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSoftUpgrade.Appearance.ForeColor = System.Drawing.Color.White;
+            this.btnSoftUpgrade.Appearance.Options.UseBackColor = true;
             this.btnSoftUpgrade.Appearance.Options.UseFont = true;
+            this.btnSoftUpgrade.Appearance.Options.UseForeColor = true;
             this.btnSoftUpgrade.Appearance.Options.UseTextOptions = true;
             this.btnSoftUpgrade.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
             this.btnSoftUpgrade.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap;
-            this.btnSoftUpgrade.Location = new System.Drawing.Point(9, 351);
+            this.btnSoftUpgrade.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.btnSoftUpgrade.Location = new System.Drawing.Point(21, 217);
             this.btnSoftUpgrade.Name = "btnSoftUpgrade";
             this.btnSoftUpgrade.Size = new System.Drawing.Size(210, 110);
             this.btnSoftUpgrade.TabIndex = 12;
@@ -103,23 +98,30 @@
             // picImg
             // 
             this.picImg.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.picImg.Location = new System.Drawing.Point(286, 74);
+            this.picImg.Image = ((System.Drawing.Image)(resources.GetObject("picImg.Image")));
+            this.picImg.Location = new System.Drawing.Point(187, 13);
             this.picImg.Name = "picImg";
-            this.picImg.Size = new System.Drawing.Size(338, 247);
+            this.picImg.Size = new System.Drawing.Size(356, 168);
+            this.picImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picImg.TabIndex = 7;
             this.picImg.TabStop = false;
             // 
             // btnExit
             // 
             this.btnExit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnExit.Appearance.BackColor = System.Drawing.Color.Blue;
             this.btnExit.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Appearance.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Appearance.Options.UseBackColor = true;
             this.btnExit.Appearance.Options.UseFont = true;
+            this.btnExit.Appearance.Options.UseForeColor = true;
             this.btnExit.Appearance.Options.UseTextOptions = true;
             this.btnExit.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
             this.btnExit.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap;
-            this.btnExit.Location = new System.Drawing.Point(691, 351);
+            this.btnExit.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.btnExit.Location = new System.Drawing.Point(271, 356);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(210, 110);
+            this.btnExit.Size = new System.Drawing.Size(174, 74);
             this.btnExit.TabIndex = 14;
             this.btnExit.Text = "Exit";
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
@@ -128,10 +130,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(913, 537);
+            this.ClientSize = new System.Drawing.Size(710, 447);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.labelControl2);
-            this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.btnLogon);
             this.Controls.Add(this.btnRemote);
             this.Controls.Add(this.btnSoftUpgrade);
@@ -143,14 +143,10 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.picImg)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private DevExpress.XtraEditors.LabelControl labelControl2;
-        private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.SimpleButton btnLogon;
         private DevExpress.XtraEditors.SimpleButton btnRemote;
         private DevExpress.XtraEditors.SimpleButton btnSoftUpgrade;
